d0919520968eae4be59575ed52120bcec8d8a5a9
Nihal George <nihalgeorge@Nihals-MacBook-Pro.local>
2017-04-08 22:31:37 -0700

    finished server

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import base64
+from PIL import Image
+import io
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/upload', methods=['POST']) 
+def upload():
+	string = base64.b64decode(request.form['image'])
+	image = Image.open(io.BytesIO(string))
+	return "nothing";
+
 if __name__ == "__main__":
     app.run()
+
+
+
